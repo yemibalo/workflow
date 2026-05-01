5473df19184940684ff5ede0e3575009a352476c
yemibalo <yettybalo62@gmail.com>
2026-05-01 18:54:02 +0100

    fix: correct file structure and test

--- math.test.js
+++
@@ -1,9 +0,0 @@
-const { add } = require('../src/math');
-
-test('adds two numbers correctly', () => {
-  expect(add(2, 3)).toBe(5);
-});
-
-test('adds negative numbers', () => {
-  expect(add(-1, 1)).toBe(0);
-});
